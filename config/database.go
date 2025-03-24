1e916ecfbe880aeeb38a8a49da9a98441dddaf8c
Lukman Hadi <lukmanhadi2000@gmail.com>
2025-03-24 21:53:03 +0700

    add auto migrate in golang

--- config/database.go
+++ config/database.go
@@ -4,31 +4,39 @@ import (
 	"log"
 	"os"
 
+	"mahasiswa-api-golang/model"
+
 	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 func DatabaseConnection() *gorm.DB {
-	// Load file .env
+
 	err := godotenv.Load()
 	if err != nil {
 		log.Println("No .env file found, using system environment variables")
 	}
 
-	// Ambil DATABASE_URL dari environment variables
 	dsn := os.Getenv("DATABASE_URL")
 
 	if dsn == "" {
 		log.Fatal("DATABASE_URL not found in environment variables")
 	}
 
-	// Koneksi ke PostgreSQL
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("Failed to connect to database: %v", err)
 	}
 
 	log.Println("Database connection established successfully!")
+
+	err = db.AutoMigrate(&model.Students{})
+	if err != nil {
+		log.Fatalf("Failed to migrate database: %v", err)
+	}
+
+	log.Println("Database migration completed successfully!")
+
 	return db
 }
